2cf61ab0cc172feb60cf884eb614ab28adab6d23
Virginia Villalobos <virginiavt98@gmail.com>
2023-06-06 18:18:35 +0200

    layout

---
+++ javascript.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('nav');
+    var scrollPosition = window.scrollY;
+    if (scrollPosition > window.innerHeight) { // if we've scrolled past the height of the window (i.e., past the hero section)
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
